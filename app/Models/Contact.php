e419a35bab8f4348132fcb41a5fb378c57aa32d3
YoldashevFayzulla <yoldashevfayzulla0@gmail.com>
2023-05-19 22:57:43 +0500

    error not fixed in contact tables

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -9,6 +9,8 @@ class Contact extends Model
 {
     use HasFactory;
     protected $fillable=['name','number','massage','book_id'];
+
+//    error
     public function post(){
         return $this->belongsTo(Post::class,    'book_id','id');
     }
